1e28480dda219760631cd2829562ce2c29213eaf
Ubuntu <ubuntu@ip-172-31-11-21.us-west-2.compute.internal>
2013-07-05 13:28:47 +0000

    Added grader.js

---
+++ grader.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var restler = require('restler');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://vast-anchorage-9853.herokuapp.com/index.html";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var assertValidUrl = function(url) {
+  var urlstring = url.toString();
+  reslter.get(urlstring).on('complete', function(result)
+                      {
+                        if (result instanceof Error)
+                        {
+                          console.log("%s in an invalid url. Exiting.", url);
+                          process.exit(1);
+                        }
+                        return url;
+                      }
+                     )
+};
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var restlerUrl = function(url) {
+  var urlstring = url.toString();
+  restler.get(urlstring).on('complete', function(result)
+                      {
+console.log(result);
+                        return result;
+                      }
+                     )
+};
+
+var checkHtmlFile = function(htmlfile, url, checksfile) {
+    if (url != null)
+    {
+      fs.writeFileSync("index.htm",restlerUrl(url));
+      htmlfile = "index.htm";
+    }
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+if(require.main == module) {
+    program
+        .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
+        .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-u, --url ', 'URL to index.html'/*, assertValidUrl, URL_DEFAULT*/)
+        .parse(process.argv);
+    var checkJson =  checkHtmlFile(program.file, program.url, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
